e5570b691349c1b5b829fd49ed3717f44b54feeb
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-10-13 22:19:28 +0200

    added code to remove class
    added code to remove class

--- open-menu.js
+++ open-menu.js
@@ -1,25 +1,31 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileMenuscroll = document.querySelector("[data-scroll]");
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileMenuscroll = document.querySelector("[data-scroll]");
 
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-scroll");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-      mobileMenuscroll.classList.toggle("is-scroll");
+  menuBtnRef.addEventListener("click", () => {
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true";
+
+    menuBtnRef.classList.toggle("is-scroll");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    mobileMenuscroll.classList.toggle("is-scroll");
+  });
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true";
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  // Исправленный селектор для ссылок с классом header-mob-link
+  document.querySelectorAll('.header-mob-link').forEach(link => {
+    link.addEventListener('click', (event) => {
+      // Снимаем классы при клике на ссылку
+      mobileMenuscroll.classList.remove("is-scroll");
+      mobileMenuRef.classList.remove("is-open");
     });
-  
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+  });
+})();
