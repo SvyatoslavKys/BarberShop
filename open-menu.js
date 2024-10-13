e6b0f8c13e416b1e8bac411e4fe14dcaea1c8290
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-10-13 22:04:35 +0200

    added scroll mobile menu
    added scroll mobile menu

--- open-menu.js
+++ open-menu.js
@@ -1,6 +1,17 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const mobileMenuscroll = document.querySelector("[data-scroll]");
+
+    menuBtnRef.addEventListener("click", () => {
+      const expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  
+      menuBtnRef.classList.toggle("is-scroll");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+      mobileMenuscroll.classList.toggle("is-scroll");
+    });
+  
   
     menuBtnRef.addEventListener("click", () => {
       const expanded =
